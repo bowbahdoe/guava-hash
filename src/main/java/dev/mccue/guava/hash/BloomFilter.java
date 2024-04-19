7f8146dfae80c394fbddef5f480d8e96e38673b3
Ethan McCue <ethan@mccue.dev>
2024-04-19 11:28:49 -0400

    Bump to 33.1.0

--- src/main/java/dev/mccue/guava/hash/BloomFilter.java
+++ src/main/java/dev/mccue/guava/hash/BloomFilter.java
@@ -590,6 +590,7 @@ public final class BloomFilter<T extends @Nullable Object> implements Predicate<
    * @throws IOException if the InputStream throws an {@code IOException}, or if its data does not
    *     appear to be a BloomFilter serialized using the {@code #writeTo(OutputStream)} method.
    */
+  @SuppressWarnings("CatchingUnchecked") // sneaky checked exception
   public static <T extends @Nullable Object> BloomFilter<T> readFrom(
       InputStream in, Funnel<? super T> funnel) throws IOException {
     checkNotNull(in, "InputStream");
@@ -614,7 +615,9 @@ public final class BloomFilter<T extends @Nullable Object> implements Predicate<
       }
 
       return new BloomFilter<T>(dataArray, numHashFunctions, funnel, strategy);
-    } catch (RuntimeException e) {
+    } catch (IOException e) {
+      throw e;
+    } catch (Exception e) { // sneaky checked exception
       String message =
           "Unable to deserialize BloomFilter from InputStream."
               + " strategyOrdinal: "
@@ -626,4 +629,6 @@ public final class BloomFilter<T extends @Nullable Object> implements Predicate<
       throw new IOException(message, e);
     }
   }
+
+  private static final long serialVersionUID = 0xcafebabe;
 }
